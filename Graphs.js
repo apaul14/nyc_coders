d9eb7ef884063d7ad2adb406b23160c770394c49
Alex Paul <al.9thousand@gmail.com>
2020-02-27 13:57:19 -0500

    finished removeedge

--- Graphs.js
+++ Graphs.js
@@ -5,13 +5,18 @@ class Graph {
   addVertex(vertex){
     this.adjacencyList[vertex] = []
   }
-  addEdge(vert1, vert2){
+  addEdge(vert1, vert2) {
     if (!this.adjacencyList[vert1].includes(vert2)) {
       this.adjacencyList[vert1].push(vert2)
     }
-   
   }
-}
+  removeEdge(vert1, vert2) {
+    if (this.adjacencyList[vert1].includes(vert2)) {
+      this.adjacencyList[vert1].splice(this.adjacencyList[vert1].indexOf(vert2),1)
+     }
+    } 
+  }
+
 
 let graph = new Graph
 
@@ -20,6 +25,10 @@ graph.addVertex(2)
 graph.addVertex(3)
 graph.addEdge(1,2)
 graph.addEdge(1,3)
+
+console.log(graph)
+
+graph.removeEdge(1,3)
 
 
 console.log(graph)
